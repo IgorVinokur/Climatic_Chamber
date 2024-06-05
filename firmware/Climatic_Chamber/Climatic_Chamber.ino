df71c9d82fbe71ed69bbdc41c57a262d7f910aa8
IgorVinokur <igorvin2@gmail.com>
2024-06-05 21:40:08 +0300

    Structure Changes

---
+++ firmware/Climatic_Chamber/Climatic_Chamber.ino
@@ -0,0 +1,60 @@
+#include "data.h"
+
+void setup() {
+
+  startup();
+ 
+  
+
+  ntp.begin();
+  ntp.setGMT(mydata.gmt);         // часовой пояс. Для Москвы: 3
+ byte count = 0;
+  while (!ntp.synced()) {
+    ntp.updateNow();
+    delay(1000);
+    if (++count > 10) break;
+  }
+
+  bme280Init();
+  //EEPROM.begin(100);     // выделить память (больше или равно размеру структуры данных)
+  //eemem.begin(0, 'e');  // изменить букву в скобках на другую, чтобы восстановить настройки по умолчанию
+
+  
+  pinMode(RELE_TEMP, OUTPUT);
+  pinMode(RELE_THUM, OUTPUT);
+  pinMode(RELE_DRAIN, OUTPUT);
+  pinMode(RELE_C_VENTA, OUTPUT);
+  pinMode( RELE_Q_LAMP, OUTPUT);
+ 
+ // if (data.rele_1_isOn) digitalWrite(RELE1, ON);
+ // else digitalWrite(RELE1, OFF);
+ // digitalWrite(RELE2, OFF);
+
+}  //setup()
+
+
+void loop() {
+ // eemem.tick();
+  data.tick(); 
+  ui.tick();
+  ntp.tick();
+  eb.tick();
+  temprelay();
+  
+
+  // раз в 1 сек делаем дела
+  static uint32_t ms1 = 0;
+  if (millis() - ms1 >= 1000) {
+    ms1 = millis();
+
+    bme280Read();
+
+    // отдаем текущую дату и время переменным в веб интерфейс
+    nowTime.set(ntp.hour(), ntp.minute(), ntp.second());
+    nowDate.set(ntp.year(), ntp.month(), ntp.day());
+    // если разрешено, включаем по времени нагрузку ( свет или насос)
+
+    
+
+  }
+}  //loop()
---
+++ firmware/Climatic_Chamber/startup.ino
@@ -0,0 +1,89 @@
+#define CONNECT_TIMEOUT 10000
+
+void startup() {
+ // EEPROM.begin(eemem.blockSize());
+ // eemem.begin(0, 'b');  // изменить букву в скобках на другую, чтобы восстановить настройки по умолчанию
+
+  Serial.begin(115200);
+  while (!LittleFS.begin()) {           // Инициализация файловой системы
+    LittleFS.format();
+  }
+
+
+  if (!LittleFS.begin()) Serial.println("FS Error");
+
+
+  // прочитать данные из файла в переменную
+  // при первом запуске в файл запишутся данные из структуры
+  FDstat_t stat = data.read();
+
+  switch (stat) {
+    case FD_FS_ERR: Serial.println("FS Error");
+      break;
+    case FD_FILE_ERR: Serial.println("Error");
+      break;
+    case FD_WRITE: Serial.println("Data Write");
+      break;
+    case FD_ADD: Serial.println("Data Add");
+      break;
+    case FD_READ: Serial.println("Data Read");
+      break;
+    default:
+      break;
+  }
+
+  Serial.println("Data read:");
+
+
+  WiFi.mode(WIFI_STA);
+  Serial.println(String("Connecting ") + mydata.staSsid + ',' + mydata.staPass);
+  WiFi.begin(mydata.staSsid, mydata.staPass);
+  uint32_t tmr = millis();
+  bool fail = false;
+  while (WiFi.status() != WL_CONNECTED) {
+    if (millis() - tmr >= CONNECT_TIMEOUT) {
+      fail = 1;
+      break;
+    }
+  
+ 
+    Serial.print(".");
+    delay(500);
+  }
+
+  if (fail) {
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(mydata.apSsid, mydata.apPass);  // Create AP
+    Serial.println("Fail! AP mode");
+    Serial.println(WiFi.softAPIP());
+  } else {
+    Serial.println(WiFi.localIP());
+    //ClientIP = WiFi.localIP().toString().c_str();
+  }
+
+  temp_relay_heating.setpoint = mydata.temp;        // установка (ставим на 40 градусов)
+  temp_relay_heating.hysteresis = mydata.temp_hys;  // ширина гистерезиса
+  temp_relay_heating.k = 0.5;                          // коэффициент обратной связи (подбирается по факту)
+
+  temp_relay_cooling.setpoint = mydata.temp;        // установка (ставим на 40 градусов)
+  temp_relay_cooling.hysteresis = mydata.temp_hys;  // ширина гистерезиса
+  temp_relay_cooling.k = 0.5;                          // коэффициент обратной связи (подбирается по факту)
+
+
+
+  //Encoder Settings
+  eb.setBtnLevel(LOW);
+  eb.setClickTimeout(500);
+  eb.setDebTimeout(50);
+  eb.setHoldTimeout(600);
+  eb.setStepTimeout(200);
+
+  eb.setEncReverse(0);
+  eb.setEncType(EB_STEP4_LOW);
+  eb.setFastTimeout(30);
+
+  ui.start("cl-chamber");
+  ui.attachBuild(build);
+  ui.attach(action);
+  ui.enableOTA();
+}
