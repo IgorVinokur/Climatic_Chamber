4ef5b769bbbef23446fff54d2d959c743906788b
IgorVinokur <igorvin2@gmail.com>
2024-07-13 00:07:43 +0300

    Add Display Menu

--- firmware/Climatic_Chamber/display.ino
+++ firmware/Climatic_Chamber/display.ino
@@ -11,12 +11,13 @@ void displayInit() {
   tft.println("Climatic Chamber v0.1 ");
   delay(5000);
   tft.fillScreen(ST77XX_BLACK);
-  MainDisplay();
+  displayMainScreen();
+  lastInteractionTime = millis();  // Initialize last interaction time
 }
 
 
-void MainDisplay() {
-  mainDisplay = 1;
+void displayMainScreen() {
+  tft.fillScreen(ST77XX_BLACK);
   tft.setTextColor(ST77XX_WHITE);
   tft.setTextSize(2);
   tft.setCursor(130, 10);
@@ -24,15 +25,15 @@ void MainDisplay() {
   tft.drawLine(1, 30, 320, 30, ST77XX_YELLOW);
   tft.drawLine(220, 31, 220, 170, ST77XX_GREEN);
   tft.drawLine(220, 100, 320, 100, ST77XX_GREEN);
-  if (WiFi.getMode() == WIFI_AP){
+  if (WiFi.getMode() == WIFI_AP) {
     tft.drawBitmap(5, 8, nowireless, 20, 20, ST77XX_RED);
-    tft.drawBitmap(40, 8, nocloud, 20, 20, ST77XX_RED);
-    } else {
-      tft.drawBitmap(5, 8, wireless, 20, 20, ST77XX_GREEN);
-      tft.drawBitmap(40, 8, cloud, 20, 20, ST77XX_BLUE);
-    }
+    tft.drawBitmap(40, 8, nocloud, 30, 20, ST77XX_RED);
+  } else {
+    tft.drawBitmap(5, 8, wireless, 20, 20, ST77XX_GREEN);
+    tft.drawBitmap(40, 8, cloud, 30, 20, ST77XX_BLUE);
+  }
 
-  
+
   //tft.setCursor(10, 80);
   //tft.println("Humidity: " + String(humidity) + " %");
   //tft.fillScreen(ST77XX_BLACK);
@@ -47,21 +48,11 @@ void clockDisplay() {
 }
 
 void sensorsDisplay() {
- // float previusTemp;
- // float previusHum;
-  //tft.setTextColor(ST77XX_BLACK);
-  //tft.setCursor(235, 72);
-  //tft.setTextSize(3);
-  //tft.printf("%.2fC", previusTemp);
-  //tft.println(String(previusTemp));
-  //tft.setCursor(240, 140);
-  //tft.printf("%.1f%%", previusHum);
-  //tft.println(String(previusHum));
   tft.setTextColor(ST77XX_WHITE);
   tft.setTextSize(3);
   tft.setCursor(230, 35);
   tft.println("Temp");
-   tft.fillRect(240, 65, 40, 30, ST77XX_BLACK); //clear Temperature value
+  tft.fillRect(240, 65, 40, 30, ST77XX_BLACK);  //clear Temperature value
   tft.setTextColor(ST77XX_WHITE);
   tft.setCursor(240, 72);
   //previusTemp = temperature;
@@ -70,9 +61,188 @@ void sensorsDisplay() {
   tft.setCursor(235, 110);
   tft.setTextSize(3);
   tft.println("Hum");
-  tft.fillRect(240, 133, 40, 40, ST77XX_BLACK); //clear Humidity value
+  tft.fillRect(240, 133, 40, 40, ST77XX_BLACK);  //clear Humidity value
   tft.setTextColor(ST77XX_WHITE);
   tft.setCursor(240, 140);
   tft.println(String(humidity) + "%");
 }
+void displayMenuScreen() {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(10, 10);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(2);
+  tft.print("Menu");
 
+  const char* menuItems[] = { "Configuration", "Setup", "Info", "Back" };
+  for (int i = topMenuItem; i < min(topMenuItem + itemsPerPage, 4); i++) {
+    tft.setCursor(10, 40 + ((i - topMenuItem) * 30));
+    if (i == selectedMenuItem) {
+      tft.setTextColor(ST77XX_RED);
+    } else {
+      tft.setTextColor(ST77XX_WHITE);
+    }
+    tft.print(menuItems[i]);
+  }
+}
+
+void displaySubMenuScreen() {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(10, 10);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(2);
+  tft.print("Setup");
+
+  const char* subMenuItems[] = { "Param 1", "Param 2", "Param 3", "Param 4", "Back" };
+  for (int i = 0; i < 5; i++) {
+    tft.setCursor(10, 40 + (i * 30));
+    if (i == selectedSubMenuItem) {
+      tft.setTextColor(ST77XX_RED);
+    } else {
+      tft.setTextColor(ST77XX_WHITE);
+    }
+    tft.print(subMenuItems[i]);
+  }
+}
+
+void displaySetupScreen() {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(10, 10);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(2);
+  tft.print("Setup");
+
+  const char* setupItems[] = { "Temp. Offset", "Hum. Offset", "Restart", "Back" };
+  for (int i = topSubMenuItem; i < min(topSubMenuItem + itemsPerPage, 4); i++) {
+    tft.setCursor(10, 40 + ((i - topSubMenuItem) * 30));
+    if (i == selectedSubMenuItem) {
+      tft.setTextColor(ST77XX_RED);
+    } else {
+      tft.setTextColor(ST77XX_WHITE);
+    }
+    tft.print(setupItems[i]);
+  }
+}
+
+void displayConfigurationScreen() {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(10, 10);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(2);
+  tft.print("Configuration");
+
+  const char* configItems[] = { "Temperature Control", "Humidity Control", "Drainage", "Ventilation", "Quartz Lamp", "Back" };
+  for (int i = topSubMenuItem; i < min(topSubMenuItem + itemsPerPage, 6); i++) {
+    tft.setCursor(10, 40 + ((i - topSubMenuItem) * 30));
+    if (i == selectedSubMenuItem) {
+      tft.setTextColor(ST77XX_RED);
+    } else {
+      tft.setTextColor(ST77XX_WHITE);
+    }
+    tft.print(configItems[i]);
+  }
+}
+
+void displayInfoScreen() {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(10, 10);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(2);
+  tft.print("Setup");
+
+  const char* subMenuItems[] = { "Version", "IP:", "Param3", "Param 4", "Back" };
+  for (int i = 0; i < 5; i++) {
+    tft.setCursor(10, 40 + (i * 30));
+    if (i == selectedSubMenuItem) {
+      tft.setTextColor(ST77XX_RED);
+    } else {
+      tft.setTextColor(ST77XX_WHITE);
+    }
+    tft.print(subMenuItems[i]);
+  }
+}
+
+void displayTempOffsetScreen() {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(10, 10);
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.print("** Temp. Sensor Offset **");
+
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setCursor(10, 50);
+  tft.setTextSize(3);
+  tft.print("Value: ");
+  tft.print(mydata.tempOffset / 10.0, 1);  // Display as floating point with one decimal
+}
+
+void displayHumOffsetScreen() {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(10, 10);
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.print("** Hum. Sensor Offset **");
+
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setCursor(10, 50);
+  tft.setTextSize(3);
+  tft.print("Value: ");
+  tft.print(mydata.humidityOffset, 1);  // Display as floating point with one decimal
+}
+
+void displayTempControlScreen() {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(10, 10);
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.print("** Temperature Control **");
+
+  tft.setTextColor(ST77XX_WHITE);
+  const char* tempControlItems[] = { "Enable", "Set Temperature", "Set Hysteresis", "Mode" };
+  for (int i = 0; i < 4; i++) {
+    tft.setCursor(10, 40 + (i * 30));
+    if (i == selectedTempControlItem) {
+      tft.setTextColor(ST77XX_RED);
+    } else {
+      tft.setTextColor(ST77XX_WHITE);
+    }
+    tft.print(tempControlItems[i]);
+    tft.print(": ");
+    if (i == 0) {
+      tft.print(mydata.sw_temp ? "ON" : "OFF");
+    } else if (i == 1) {
+      tft.print(mydata.temp);
+    } else if (i == 2) {
+      tft.print(mydata.temp_hys);
+    } else if (i == 3) {
+      tft.print(mydata.sw_tempmode ? "Cooling" : "Heating");
+    }
+  }
+}
+
+void displayHumControlScreen() {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(10, 10);
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.print("** Humidity Control **");
+
+  tft.setTextColor(ST77XX_WHITE);
+  const char* humControlItems[] = { "Enable", "Set Humidity", "Set Hysteresis" };
+  for (int i = 0; i < 3; i++) {
+    tft.setCursor(10, 40 + (i * 30));
+    if (i == selectedHumControlItem) {
+      tft.setTextColor(ST77XX_RED);
+    } else {
+      tft.setTextColor(ST77XX_WHITE);
+    }
+    tft.print(humControlItems[i]);
+    tft.print(": ");
+    if (i == 0) {
+      tft.print(mydata.sw_humidity ? "ON" : "OFF");
+    } else if (i == 1) {
+      tft.print(mydata.set_humidity);
+    } else if (i == 2) {
+      tft.print(mydata.humidity_hys);
+    }
+  }
+}
---
+++ firmware/Climatic_Chamber/displayMenu.ino
@@ -0,0 +1,286 @@
+void displayMenu() {
+  
+  if (enc.left() || enc.right() || enc.click() || enc.hold()) {
+    lastInteractionTime = millis();  // Reset the interaction timer
+  }
+  // Check for timeout
+  if (currentScreen != MAIN && (millis() - lastInteractionTime >= timeoutDuration)) {
+    currentScreen = MAIN;
+    displayMainScreen();
+  }
+  switch (currentScreen) {
+    case MAIN:
+      if (enc.click()) {
+        currentScreen = MENU;
+        displayMenuScreen();
+      }
+      break;
+
+    case MENU:
+      if (enc.left()) {
+        selectedMenuItem--;
+        if (selectedMenuItem < 0) {
+          selectedMenuItem = 3;                        // Wrap around to the last item
+          topMenuItem = max(0, 3 - itemsPerPage + 1);  // Adjust top item for wrap-around
+        } else if (selectedMenuItem < topMenuItem) {
+          topMenuItem = selectedMenuItem;
+        }
+        displayMenuScreen();
+      } else if (enc.right()) {
+        selectedMenuItem++;
+        if (selectedMenuItem > 3) {
+          selectedMenuItem = 0;  // Wrap around to the first item
+          topMenuItem = 0;       // Adjust top item for wrap-around
+        } else if (selectedMenuItem >= topMenuItem + itemsPerPage) {
+          topMenuItem = selectedMenuItem - itemsPerPage + 1;
+        }
+        displayMenuScreen();
+      } else if (enc.click()) {
+        if (selectedMenuItem == 0) {  // If "Configuration" is selected
+          currentScreen = CONFIGURATION;
+          displayConfigurationScreen();
+        } else if (selectedMenuItem == 1) {
+          currentScreen = SETUP;
+          displaySetupScreen();
+        } else if (selectedMenuItem == 2) {
+          currentScreen = INFO;
+          displayInfoScreen();
+        } else if (selectedMenuItem == 3) {  // If the back button is selected
+          currentScreen = MAIN;
+          displayMainScreen();
+        }
+      } else if (enc.hold()) {
+        currentScreen = MAIN;
+        selectedSubMenuItem = 0;
+        displayMainScreen();
+      }
+      break;
+
+    case SETUP:
+      if (enc.left()) {
+        selectedSubMenuItem--;
+        if (selectedSubMenuItem < 0) {
+          selectedSubMenuItem = 3;  // Wrap around to the last item
+        }
+        displaySetupScreen();
+      } else if (enc.right()) {
+        selectedSubMenuItem++;
+        if (selectedSubMenuItem > 3) {
+          selectedSubMenuItem = 0;  // Wrap around to the first item
+        }
+        displaySetupScreen();
+      } else if (enc.click()) {
+        if (selectedSubMenuItem == 3) {  // If the back button is selected
+          currentScreen = MENU;
+          selectedSubMenuItem = 0;
+          displayMenuScreen();
+        } else if (selectedSubMenuItem == 0) {  // If "Temp. Offset" is selected
+          currentScreen = TEMP_OFFSET;
+         displayTempOffsetScreen();
+        } else if (selectedSubMenuItem == 1) {  // If "Temp. Offset" is selected
+          currentScreen = HUMIDITY_OFFSET;
+          displayHumOffsetScreen();
+        }
+        // Handle other submenu items similarly...
+      } else if (enc.hold()) {
+        currentScreen = MENU;
+        selectedSubMenuItem = 0;
+        displayMenuScreen();
+      }
+      break;
+
+    case TEMP_OFFSET:
+      if (enc.left()) {
+        mydata.tempOffset--;
+        displayTempOffsetScreen();
+      } else if (enc.right()) {
+        mydata.tempOffset++;
+        displayTempOffsetScreen();
+      } else if (enc.click() || enc.hold()) {
+        currentScreen = SETUP;
+        displaySetupScreen();
+      }
+      break;
+
+    case TEMP_CONTROL:
+      if (enc.left()) {
+        if (selectedTempControlItem == 0) {
+          if (mydata.sw_temp) {
+            mydata.sw_temp = false;
+          } else {
+            mydata.sw_temp = true;
+          }
+        } else if (selectedTempControlItem == 1) {
+          mydata.temp--;
+        } else if (selectedTempControlItem == 2) {
+          mydata.temp_hys--;
+        } else if (selectedTempControlItem == 3) {
+          if (mydata.sw_tempmode) {
+            mydata.sw_tempmode = false;
+          } else {
+            mydata.sw_tempmode = true;
+          }
+        }
+        displayTempControlScreen();
+      } else if (enc.right()) {
+        if (selectedTempControlItem == 0) {
+          if (mydata.sw_temp) {
+            mydata.sw_temp = false;
+          } else {
+            mydata.sw_temp = true;
+          }
+        } else if (selectedTempControlItem == 1) {
+          mydata.temp++;
+        } else if (selectedTempControlItem == 2) {
+          mydata.temp_hys++;
+        } else if (selectedTempControlItem == 3) {
+          if (mydata.sw_tempmode) {
+            mydata.sw_tempmode = false;
+          } else {
+            mydata.sw_tempmode = true;
+          }
+        }
+        displayTempControlScreen();
+      } else if (enc.click()) {
+        selectedTempControlItem++;
+        if (selectedTempControlItem > 3) {
+          selectedTempControlItem = 0;
+        }
+        displayTempControlScreen();
+      } else if (enc.hold()) {
+        currentScreen = CONFIGURATION;
+        displayConfigurationScreen();
+      }
+      break;
+
+    case HUM_CONTROL:
+      if (enc.left()) {
+        if (selectedHumControlItem == 0) {
+          if (mydata.sw_humidity) {
+            mydata.sw_humidity = false;
+          } else {
+            mydata.sw_humidity = true;
+          }
+        } else if (selectedHumControlItem == 1) {
+          mydata.set_humidity--;
+        } else if (selectedHumControlItem == 2) {
+          mydata.humidity_hys--;
+        }
+        displayHumControlScreen();
+      } else if (enc.right()) {
+        if (selectedHumControlItem == 0) {
+          if (mydata.sw_humidity) {
+            mydata.sw_humidity = false;
+          } else {
+            mydata.sw_humidity = true;
+          }
+        } else if (selectedHumControlItem == 1) {
+          mydata.set_humidity++;
+        } else if (selectedHumControlItem == 2) {
+          mydata.humidity_hys++;
+        }
+        displayHumControlScreen();
+      } else if (enc.click()) {
+        selectedHumControlItem++;
+        if (selectedHumControlItem > 2) {
+          selectedHumControlItem = 0;
+        }
+        displayHumControlScreen();
+      } else if (enc.hold()) {
+        currentScreen = CONFIGURATION;
+        displayConfigurationScreen();
+      }
+      break;
+
+    case HUMIDITY_OFFSET:
+      if (enc.left()) {
+        mydata.humidityOffset--;
+        displayHumOffsetScreen();
+      } else if (enc.right()) {
+        mydata.humidityOffset++;
+        displayHumOffsetScreen();
+      } else if (enc.click() || enc.hold()) {
+        currentScreen = SETUP;
+        displaySetupScreen();
+      }
+      break;
+
+
+
+    case CONFIGURATION:
+      if (enc.left()) {
+        selectedSubMenuItem--;
+        if (selectedSubMenuItem < 0) {
+          selectedSubMenuItem = 5;                        // Wrap around to the last item
+          topSubMenuItem = max(0, 5 - itemsPerPage + 1);  // Adjust top item for wrap-around
+        } else if (selectedSubMenuItem < topSubMenuItem) {
+          topSubMenuItem = selectedSubMenuItem;
+        }
+        displayConfigurationScreen();
+      } else if (enc.right()) {
+        selectedSubMenuItem++;
+        if (selectedSubMenuItem > 5) {
+          selectedSubMenuItem = 0;  // Wrap around to the first item
+          topSubMenuItem = 0;       // Adjust top item for wrap-around
+        } else if (selectedSubMenuItem >= topSubMenuItem + itemsPerPage) {
+          topSubMenuItem = selectedSubMenuItem - itemsPerPage + 1;
+        }
+        displayConfigurationScreen();
+      } else if (enc.click()) {
+        if (selectedSubMenuItem == 5) {  // If the back button is selected
+          currentScreen = MENU;
+          selectedSubMenuItem = 0;
+          topSubMenuItem = 0;
+          displayMenuScreen();
+        } else if (selectedSubMenuItem == 0) {  // If "Temperature Control" is selected
+          currentScreen = TEMP_CONTROL;
+          displayTempControlScreen();
+        } else if (selectedSubMenuItem == 1) {  // If "Temperature Control" is selected
+          currentScreen = HUM_CONTROL;
+          displayHumControlScreen();
+        }
+        // Add specific actions for each configuration item if needed
+      } else if (enc.hold()) {
+        currentScreen = MENU;
+        selectedSubMenuItem = 0;
+        topSubMenuItem = 0;
+        displayMenuScreen();
+      }
+      break;
+
+    case INFO:
+      if (enc.left()) {
+        selectedSubMenuItem--;
+        if (selectedSubMenuItem < 0) {
+          selectedSubMenuItem = 4;                        // Wrap around to the last item
+          topSubMenuItem = max(0, 4 - itemsPerPage + 1);  // Adjust top item for wrap-around
+        } else if (selectedSubMenuItem < topSubMenuItem) {
+          topSubMenuItem = selectedSubMenuItem;
+        }
+        displayInfoScreen();
+      } else if (enc.right()) {
+        selectedSubMenuItem++;
+        if (selectedSubMenuItem > 4) {
+          selectedSubMenuItem = 0;  // Wrap around to the first item
+          topSubMenuItem = 0;       // Adjust top item for wrap-around
+        } else if (selectedSubMenuItem >= topSubMenuItem + itemsPerPage) {
+          topSubMenuItem = selectedSubMenuItem - itemsPerPage + 1;
+        }
+        displayInfoScreen();
+      } else if (enc.click()) {
+        if (selectedSubMenuItem == 4) {  // If the back button is selected
+          currentScreen = MENU;
+          selectedSubMenuItem = 0;
+          topSubMenuItem = 0;
+          displayMenuScreen();
+        }
+      } else if (enc.hold()) {
+        currentScreen = MENU;
+        selectedSubMenuItem = 0;
+        topSubMenuItem = 0;
+        displayMenuScreen();
+      }
+      break;
+  }
+ 
+}
