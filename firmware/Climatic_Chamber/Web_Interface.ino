f93ccdc3c05bb145478ee2a633b10d38667dbc1b
IgorVinokur <igorvin2@gmail.com>
2024-07-13 23:07:06 +0300

    Add Display Menu

--- firmware/Climatic_Chamber/Web_Interface.ino
+++ firmware/Climatic_Chamber/Web_Interface.ino
@@ -1,4 +1,4 @@
-// конструктор WEB страницы
+// конструктор WEB страницыupdate
 void build() {
   GP.BUILD_BEGIN(700);
   GP.THEME(GP_DARK);
@@ -8,81 +8,73 @@ void build() {
   //GP.ONLINE_CHECK();
   GP.UPDATE("nowDate,nowTime,startTime,stopTime,releIndikator,sw_temp,sw_humidity,temp,temp_hys,sw_tempmode");
   GP.TITLE("Climatic Chamber Controller", "t1");
-  
+
   GP.HR();
 
   GP.NAV_TABS_LINKS("/,/cfg,/setup,/info,", "Home,Config,Setup,info");
 
   if (ui.uri() == "/cfg") {
     GP.GRID_RESPONSIVE(700);
-    
+
 
     GP_MAKE_FORM(
       "/cfg", "SAVE",
-      GP.RELOAD_CLICK("sw_temp,mydata.sw_humidity,sw_drainage,sw_a_circulation,sw_ventilation,sw_q_lamp,sw_mqtt"); 
-      GP.UPDATE("mydata.sw_temp");
+      GP.RELOAD_CLICK("sw_temp,sw_humidity,sw_draining,sw_a_circulation,sw_ventilation,sw_q_lamp,sw_mqtt");
       GP_MAKE_GRID(
-      GP_MAKE_BLOCK_THIN_TAB(
-        "Temperature Control °С",
-        if (mydata.sw_temp) {
-            GP_MAKE_BOX(GP.PLAIN("Temp. °С:"); GP.SPINNER("temp", mydata.temp, 5, 30, 1, 1, GP_GREEN, "60pix" ));
+        GP_MAKE_BLOCK_THIN_TAB(
+          "Temperature Control °С",
+          if (mydata.sw_temp) {
+            GP_MAKE_BOX(GP.PLAIN("Temp. °С:"); GP.SPINNER("temp", mydata.temp, 5, 30, 1, 1, GP_GREEN, "60pix"));
             GP_MAKE_BOX(GP.PLAIN("Hysteresis °C:"); GP.SPINNER("temp_hys", mydata.temp_hys, 0, 10, 1, 1, GP_GREEN, "60pix"));
             GP_MAKE_BOX(GP.PLAIN("Heating Mode: "); GP.SWITCH("sw_tempmode", mydata.sw_tempmode));
-            } 
-        GP.PLAIN("Enable:");GP.SWITCH("sw_temp", mydata.sw_temp););
-        
+          } GP.PLAIN("Enable:");
+          GP.SWITCH("sw_temp", mydata.sw_temp););
 
-      GP_MAKE_BLOCK_THIN_TAB(
-        "Humidity Control %",
-        if (mydata.sw_humidity) {
-            GP_MAKE_BOX(GP.PLAIN("Humidity %:"); GP.SPINNER("set_humidity", mydata.set_humidity, 40, 90, 1, 0, GP_GREEN, "60pix" )); 
-            GP_MAKE_BOX(GP.PLAIN("Hysteresis %:  "); GP.SPINNER("humidity_hys", mydata.humidity_hys, 0, 10, 1, 0, GP_GREEN, "60pix" ));
-            } 
-        GP.PLAIN("Enable:"); GP.SWITCH("sw_humidity", mydata.sw_humidity);
-        );
-        );
+
+        GP_MAKE_BLOCK_THIN_TAB(
+          "Humidity Control %",
+          if (mydata.sw_humidity) {
+            GP_MAKE_BOX(GP.PLAIN("Humidity %:"); GP.SPINNER("set_humidity", mydata.set_humidity, 40, 90, 1, 0, GP_GREEN, "60pix"));
+            GP_MAKE_BOX(GP.PLAIN("Hysteresis %:  "); GP.SPINNER("humidity_hys", mydata.humidity_hys, 0, 10, 1, 0, GP_GREEN, "60pix"));
+          } GP.PLAIN("Enable:");
+          GP.SWITCH("sw_humidity", mydata.sw_humidity);););
 
       GP_MAKE_GRID(
-      GP_MAKE_BLOCK_THIN_TAB(
-        "Drainage %",
-        if (mydata.sw_drainage) {
-        GP_MAKE_BOX(GP.PLAIN("Drainage %:"); GP.SPINNER("drainage", mydata.drainage, 40, 90, 1, 0, GP_GREEN, "60pix"));
-        GP_MAKE_BOX(GP.PLAIN("Hysteresis %:"); GP.SPINNER("drainage_hys", mydata.drainage_hys, 0, 10, 1, 0, GP_GREEN, "60pix"));
-        }
-        GP.PLAIN("Enable:"); GP.SWITCH("sw_drainage", mydata.sw_drainage););
+        GP_MAKE_BLOCK_THIN_TAB(
+          "Draining %",
+          if (mydata.sw_draining) {
+            GP_MAKE_BOX(GP.PLAIN("Draining %:"); GP.SPINNER("set_draining", mydata.set_draining, 40, 90, 1, 0, GP_GREEN, "60pix"));
+            GP_MAKE_BOX(GP.PLAIN("Hysteresis %:"); GP.SPINNER("draining_hys", mydata.draining_hys, 0, 10, 1, 0, GP_GREEN, "60pix"));
+          } GP.PLAIN("Enable:");
+          GP.SWITCH("sw_draining", mydata.sw_draining););
 
-      GP_MAKE_BLOCK_THIN_TAB(
-        "Air Circulation",
-        if (mydata.sw_a_circulation) {
-        GP_MAKE_BOX(GP.PLAIN("Period Hrs:");GP.SPINNER("air_circulation_Hours", mydata.air_circulation_Hours, 1, 24, 1, 0, GP_GREEN, "60pix"));
-        GP_MAKE_BOX(GP.PLAIN("Duration Min:"); GP.SPINNER("circulation_work_time", mydata.circulation_work_time, 1, 60, 1, 0, GP_GREEN, "60pix"));
-        GP_MAKE_BOX(GP.PLAIN("Fan:"); GP.SLIDER("fan_pwr", mydata.fan_pwr, 0, 100, 1));
-        }
-        GP.PLAIN("Enable:"); GP.SWITCH("sw_a_circulation", mydata.sw_a_circulation);
-      );
-      );
-      
+        GP_MAKE_BLOCK_THIN_TAB(
+          "Air Circulation",
+          if (mydata.sw_a_circulation) {
+            GP_MAKE_BOX(GP.PLAIN("Period Hrs:"); GP.SPINNER("air_circulation_Period", mydata.air_circulation_Period, 1, 24, 1, 0, GP_GREEN, "60pix"));
+            GP_MAKE_BOX(GP.PLAIN("Duration Min:"); GP.SPINNER("air_circulation_Duration", mydata.air_circulation_Duration, 1, 60, 1, 0, GP_GREEN, "60pix"));
+            GP_MAKE_BOX(GP.PLAIN("Fan:"); GP.SLIDER("air_circulation_fan_pwr", mydata.air_circulation_fan_pwr, 0, 100, 1));
+          } GP.PLAIN("Enable:");
+          GP.SWITCH("sw_a_circulation", mydata.sw_a_circulation);););
+
       GP_MAKE_GRID(
-      GP_MAKE_BLOCK_THIN_TAB(
-        "Ventilation",
-        if (mydata.sw_ventilation) {
-        GP_MAKE_BOX(GP.PLAIN("Period Hrs:"); GP.SPINNER("venta_Hours", mydata.venta_Hours, 1, 24, 1, 0, GP_GREEN, "60pix"));
-        GP_MAKE_BOX(GP.PLAIN("Duration Min:"); GP.SPINNER("venta_work_time", mydata.venta_work_time, 1, 60, 1, 0, GP_GREEN, "60pix"));
-        }
-        GP.PLAIN("Enable:"); GP.SWITCH("sw_ventilation", mydata.sw_ventilation);
-      );
+        GP_MAKE_BLOCK_THIN_TAB(
+          "Ventilation",
+          if (mydata.sw_ventilation) {
+            GP_MAKE_BOX(GP.PLAIN("Period Hrs:"); GP.SPINNER("venta_Hours", mydata.venta_Hours, 1, 24, 1, 0, GP_GREEN, "60pix"));
+            GP_MAKE_BOX(GP.PLAIN("Duration Min:"); GP.SPINNER("venta_work_time", mydata.venta_work_time, 1, 60, 1, 0, GP_GREEN, "60pix"));
+          } GP.PLAIN("Enable:");
+          GP.SWITCH("sw_ventilation", mydata.sw_ventilation););
 
-      GP_MAKE_BLOCK_THIN_TAB(
-        "Quartz Lamp",
-         if (mydata.sw_q_lamp) {
-        GP_MAKE_BOX(GP.PLAIN("Period Hrs:"); GP.SPINNER("quartz_Hours", mydata.quartz_Hours, 1, 24, 1, 0, GP_GREEN, "60pix"));
-        GP_MAKE_BOX(GP.PLAIN("Duration Min:"); GP.SPINNER("quartz_work_time", mydata.quartz_work_time, 1, 60, 1, 0, GP_GREEN, "60pix"));
-         }
-        GP.PLAIN("Enable:"); GP.SWITCH("sw_q_lamp", mydata.sw_q_lamp);
-      );
-      );
+        GP_MAKE_BLOCK_THIN_TAB(
+          "Quartz Lamp",
+          if (mydata.sw_q_lamp) {
+            GP_MAKE_BOX(GP.PLAIN("Period Hrs:"); GP.SPINNER("quartz_Hours", mydata.quartz_Hours, 1, 24, 1, 0, GP_GREEN, "60pix"));
+            GP_MAKE_BOX(GP.PLAIN("Duration Min:"); GP.SPINNER("quartz_work_time", mydata.quartz_work_time, 1, 60, 1, 0, GP_GREEN, "60pix"));
+          } GP.PLAIN("Enable:");
+          GP.SWITCH("sw_q_lamp", mydata.sw_q_lamp);););
 
-      
+
 
     );
   } else if (ui.uri() == "/setup") {
@@ -91,7 +83,7 @@ void build() {
 
     GP_MAKE_FORM(
       "/setup", "SAVE",
-      GP.RELOAD_CLICK("sw_mqtt"); 
+      GP.RELOAD_CLICK("sw_mqtt");
       GP_MAKE_BLOCK_TAB(
         "Set TimeZone",
         GP_MAKE_BOX(GP.LABEL("GMT"); GP.NUMBER("gmt", "GMT", mydata.gmt)););
@@ -101,24 +93,21 @@ void build() {
         GP_MAKE_BOX(GP.LABEL("SSID: "); GP.TEXT("ssid", "SSID", mydata.staSsid));
         GP_MAKE_BOX(GP.LABEL("Pass: "); GP.PASS_EYE("pass", "Password", mydata.staPass));
         GP_MAKE_BOX(GP.LABEL("AP SSID: "); GP.TEXT("ap_ssid", "AP SSID", mydata.apSsid));
-        GP_MAKE_BOX(GP.LABEL("AP Pass: "); GP.TEXT("ap_pass", "AP Password", mydata.apPass));
-        );
+        GP_MAKE_BOX(GP.LABEL("AP Pass: "); GP.TEXT("ap_pass", "AP Password", mydata.apPass)););
 
       GP_MAKE_BLOCK_TAB(
         "MQTT Settings",
-         if (mydata.sw_mqtt) {
-        GP_MAKE_BOX(GP.LABEL("MQTT Server: "); GP.TEXT("mqttServer", "MQTT Server", mydata.mqttServer));
-        GP_MAKE_BOX(GP.LABEL("MQTT Port:"); GP.NUMBER("mqttPort", "MQTT Port 1883", mydata.mqttPort));
-        GP_MAKE_BOX(GP.LABEL("MQTT UserName:"); GP.TEXT("mqttUser", "MQTT User", mydata.mqttUser));
-        GP_MAKE_BOX(GP.LABEL("MQTT Password:"); GP.PASS_EYE("mqttPwd", "MQTT Password", mydata.mqttPwd));
-        GP_MAKE_BOX(GP.LABEL("MQTT topic: "); GP.TEXT("mqttTopic", "MQTT Topic", mydata.mqttTopic));
+        if (mydata.sw_mqtt) {
+          GP_MAKE_BOX(GP.LABEL("MQTT Server: "); GP.TEXT("mqttServer", "MQTT Server", mydata.mqttServer));
+          GP_MAKE_BOX(GP.LABEL("MQTT Port:"); GP.NUMBER("mqttPort", "MQTT Port 1883", mydata.mqttPort));
+          GP_MAKE_BOX(GP.LABEL("MQTT UserName:"); GP.TEXT("mqttUser", "MQTT User", mydata.mqttUser));
+          GP_MAKE_BOX(GP.LABEL("MQTT Password:"); GP.PASS_EYE("mqttPwd", "MQTT Password", mydata.mqttPwd));
+          GP_MAKE_BOX(GP.LABEL("MQTT topic: "); GP.TEXT("mqttTopic", "MQTT Topic", mydata.mqttTopic));
+        } GP.PLAIN("Enable:");
+        GP.SWITCH("sw_mqtt", mydata.sw_mqtt);););
 
-         }
-        GP.PLAIN("Enable:"); GP.SWITCH("sw_mqtt", mydata.sw_mqtt);
-      ););
-      
 
-    
+
     GP_MAKE_BLOCK_THIN_TAB(
       "OTA Firmware",
       GP_MAKE_BOX(
@@ -127,31 +116,32 @@ void build() {
         GP.OTA_FILESYSTEM();););
     //все обновляющиеся параметры на WEB странице надо указать тут
     GP_MAKE_BLOCK_THIN_TAB(
-        "Calibration Offsets",
-        GP_MAKE_BOX(GP.PLAIN("Temp. Offset °C");GP.SPINNER("tempOffset", mydata.tempOffset, -10, 10, 1, 1, GP_GREEN, "60pix"));
-        GP_MAKE_BOX(GP.PLAIN("Humidity Offset %"); GP.SPINNER("humidityOffset", mydata.humidityOffset, 0, 20, 1, 0, GP_GREEN, "60pix"));
+      "Calibration Offsets",
+      GP_MAKE_BOX(GP.PLAIN("Temp. Offset °C"); GP.SPINNER("tempOffset", mydata.tempOffset, -10, 10, 1, 1, GP_GREEN, "60pix"));
+      GP_MAKE_BOX(GP.PLAIN("Humidity Offset %"); GP.SPINNER("humidityOffset", mydata.humidityOffset, 0, 20, 1, 0, GP_GREEN, "60pix"));
 
-      );
+    );
+
     GP_MAKE_BLOCK_THIN_TAB(
-        "Reset",
-        GP_MAKE_BOX(GP.BUTTON_MINI("espRestart", "Restart"));
-      ); 
+      "Reset",
+      GP_MAKE_BOX(GP.BUTTON_MINI("espRestart", "Restart")););
 
 
   } else if (ui.uri() == "/info") {
 
     GP_MAKE_BLOCK_TAB(
       "Connection Info",
-     // GP.LABEL("WiFi mode: ");
-     // GP.LABEL_BLOCK(WiFi.getMode() == WIFI_AP ? "AP" : "STA"); GP.BREAK(); GP.BREAK();
+      // GP.LABEL("WiFi mode: ");
+      // GP.LABEL_BLOCK(WiFi.getMode() == WIFI_AP ? "AP" : "STA"); GP.BREAK(); GP.BREAK();
 
-     // GP.LABEL("Time synced");
+      // GP.LABEL("Time synced");
       //GP.LED("leds", ntp.synced()); GP.BREAK();
-      
+
       //GP.TIME("nowTime", nowTime);
       //GP.DATE("nowDate", nowDate);
-      GP.SYSTEM_INFO(); GP.BREAK(););
-      
+      GP.SYSTEM_INFO();
+      GP.BREAK(););
+
 
 
 
