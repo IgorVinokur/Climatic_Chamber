a28b76c7ff0a08e5ae66a3cc511756a01c499555
IgorVinokur <igorvin2@gmail.com>
2024-07-28 00:32:19 +0300

    General changes

--- firmware/Climatic_Chamber_v2/Climatic_Chamber_v2.ino
+++ firmware/Climatic_Chamber_v2/Climatic_Chamber_v2.ino
@@ -3,16 +3,27 @@
 #include "config.h"
 #include "db.h"
 #include "hub.h"
-#include "wifi.h"
-
-
 
 void setup() {
+
   Serial.begin(115200);
 
-   db_init();
-   wifi_Init(); 
- 
+
+  db_init();
+  wifi_Init();
+
+  
+  ntp.setGMT(mydata.gmt);  // часовой пояс. Для Москвы: 3
+  ntp.setHost(mydata.ntp_srv);
+  ntp.begin();
+  byte count = 0;
+  while (!ntp.synced()) {
+    ntp.updateNow();
+    delay(1000);
+   if (++count > 10) break;
+  }
+  
+
   hub.onBuild(build);
   hub.begin();
   hub.setBufferSize(2000);
@@ -21,13 +32,12 @@ void setup() {
 
 void loop() {
 
+  data.tick();
   hub.tick();
-    
+  ntp.tick();
 
-
+if (ntp.ms() == 500) {
+   formattedDate = ntp.timeString();
+  }
+ 
 }
-
-
-
-
-
